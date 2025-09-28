44ae94202b8432c07a038e681e45eb37ef3b05ff
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2025-09-28 14:09:58 -0500

    Inicializacion del repositorio

---
+++ app/VoltTests/HomeTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\VoltTests;
+
+use VoltTest\Laravel\Contracts\VoltTestCase;
+use VoltTest\Laravel\VoltTestManager;
+
+class HomeTest implements VoltTestCase
+{
+    /**
+     * Define the test scenario.
+     *
+     * @param VoltTestManager $manager
+     * @return void
+     */
+    public function define(VoltTestManager $manager): void
+    {
+        // Define your test scenario
+        $scenario = $manager->scenario('HomeTest');
+        $scenario->step('Check route')
+                ->get('/home')
+                ->expectStatus(200)
+                ;
+
+// No routes selected
+    }
+}
