5f4085d30f3e4a1c3bb55cf5a9240eb2abea3a95
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2025-10-20 11:56:03 -0500

    Agrega mas filtros

--- resources/js/components/publications/publication-card.tsx
+++ resources/js/components/publications/publication-card.tsx
@@ -1,6 +1,7 @@
 import { publicationView } from "@/routes";
 import { Publication } from "@/types";
 import { Link } from "@inertiajs/react";
+import { Label } from "../ui/label";
 
 interface PublicationCardProps {
     publication: Publication
@@ -20,6 +21,14 @@ export default function PublicationCard({ publication }: PublicationCardProps) {
                 />
                 <h3 className="font-semibold text-lg">{publication.title}</h3>
                 <p className="text-gray-600 flex-grow">{publication.description}</p>
+                <div className="flex flex-row gap-2">
+                    <Label className="bg-amber-400 rounded-2xl w-fit p-1.5">
+                        <p>{publication.category.name}</p>
+                    </Label>
+                    <Label className="bg-amber-400 rounded-2xl w-fit p-1.5">
+                        <p>{publication.type}</p>
+                    </Label>
+                </div>
                 <p className="text-blue-600 font-bold mt-2">${publication.price}</p>
                 <button>Peek</button>
             </div>
