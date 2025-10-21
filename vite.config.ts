04e434294d8e395e734da4eba296730dedf30840
Elkinnn <elkinjoao2004@hotmail.com>
2025-10-21 01:56:27 -0500

    Se ajustan los contenedores.

--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,14 @@ export default defineConfig({
     esbuild: {
         jsx: 'automatic',
     },
+    server: {
+        host: '0.0.0.0',     
+        port: 5173,          
+        watch: {
+            usePolling: true, 
+        },
+        hmr: {
+            host: 'localhost', 
+        },
+    },
 });
