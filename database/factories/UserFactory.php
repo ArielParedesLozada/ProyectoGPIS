4c99bfe5c14c05a504c149c17c74e4f32e2059d9
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2025-10-24 00:35:38 -0500

    Agrega pruebas de login y de register. // TODO: pruebas de Email, Verificacion, Dashboard, Gestion de Producto, Gestion de Usuarios

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Enums\GenderType;
+use App\Enums\RoleType;
+use App\Enums\StatusType;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -24,11 +27,21 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'cedula' => fake()->unique()->numerify('##########'),
+            'name' => fake()->firstName(),
+            'surname' => fake()->lastName(),
+            'phone' => fake()->phoneNumber(),
+            'address' => fake()->address(),
+            'gender' => fake()->randomElement(GenderType::cases()),
+            'status' => fake()->randomElement(StatusType::cases()),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'deleted_at' => null,
+            'is_active' => true,
+            'role' => RoleType::ADMIN,
         ];
     }
 
@@ -37,7 +50,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
