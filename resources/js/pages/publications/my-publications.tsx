c81783940437b667f22e1fc0031b2e25924b8837
Diego200509 <diegojijon2005@gmail.com>
2025-10-24 20:48:54 -0500

    Se implemento mejoras en los icnos de mis publicaciones

--- resources/js/pages/publications/my-publications.tsx
+++ resources/js/pages/publications/my-publications.tsx
@@ -323,7 +323,9 @@ function MyPublicationsContent() {
 
                                         {/* Location */}
                                         <div className="flex items-center text-sm text-gray-500 mb-2">
-                                            <MapPin className="w-4 h-4 mr-1 flex-shrink-0" />
+                                            <svg className="w-4 h-4 text-red-500 mr-1 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
+                                                <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
+                                            </svg>
                                             <ConditionalTooltip 
                                                 content={publication.location || ''}
                                                 className="truncate flex-1 min-w-0"
@@ -343,7 +345,9 @@ function MyPublicationsContent() {
 
                                         {/* Date */}
                                         <div className="flex items-center text-xs text-gray-500">
-                                            <Calendar className="w-3 h-3 mr-1" />
+                                            <svg className="w-3 h-3 text-blue-500 mr-1 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
+                                                <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd" />
+                                            </svg>
                                             <span>
                                                 {new Date(publication.published_at).toLocaleDateString()}
                                             </span>
